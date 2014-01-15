8f8fb9d8c3bbf02bab885720645d2067c475b6d6
Daniel Wegener <daniel.wegener@holisticon.de>
2014-01-15 19:28:10 +0100

    add integrationtests with openejb to trace-jms

--- jms/src/main/java/de/holisticon/util/tracee/jms/TraceeMessageListener.java
+++ jms/src/main/java/de/holisticon/util/tracee/jms/TraceeMessageListener.java
@@ -5,6 +5,7 @@ import de.holisticon.util.tracee.TraceeConstants;
 
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import java.lang.reflect.Method;
 import java.util.TreeMap;
@@ -20,24 +21,35 @@ public class TraceeMessageListener {
     public Object intercept(InvocationContext ctx) throws Exception {
         final boolean provideContext = isMessageListenerOnMessageMethod(ctx.getMethod());
 
+        final Message message = extractParameter(ctx.getParameters());
         try {
-            final Message message = extractParameter(ctx.getParameters());
-            final Object encodedTraceeContext = message.getObjectProperty(TraceeConstants.JMS_HEADER_NAME);
-            if (encodedTraceeContext != null) {
-                final TreeMap<String, String> encodedTraceeProperties = (TreeMap<String, String>) encodedTraceeContext;
-                Tracee.getBackend().putAll(encodedTraceeProperties);
+            if (provideContext) {
+                beforeProcessing(ctx, message);
             }
             return ctx.proceed();
         } finally {
             if (provideContext) {
-                Tracee.getBackend().clear();
+                afterProcessing(ctx, message);
+
             }
         }
     }
 
+    public void beforeProcessing(InvocationContext ctx, Message message) throws JMSException {
+        final Object encodedTraceeContext = message.getObjectProperty(TraceeConstants.JMS_HEADER_NAME);
+        if (encodedTraceeContext != null) {
+            final TreeMap<String, String> encodedTraceeProperties = (TreeMap<String, String>) encodedTraceeContext;
+            Tracee.getBackend().putAll(encodedTraceeProperties);
+        }
+    }
+
+    public void afterProcessing(InvocationContext ctx, Message message) {
+        Tracee.getBackend().clear();
+    }
+
 
     Message extractParameter(Object[] parameters) {
-        if (parameters != null && parameters.length != 1 && parameters[0] instanceof Message) {
+        if (parameters != null && parameters.length == 1 && parameters[0] instanceof Message) {
             return (Message) parameters[0];
         }
         return null;
