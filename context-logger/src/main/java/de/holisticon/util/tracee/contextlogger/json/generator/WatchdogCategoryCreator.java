8cf1cc2def6b70b60c7075433a18cbb8b3b21cf4
Tobias Gindler <tobias.gindler@holisticon.de>
2014-02-20 23:54:27 +0100

    Added RecursiveReflectionToStringStyle.java for ReflectionToStringBuilder. String representation of parameter instances and instances of invoked methods will be generated up to a max depth.

--- context-logger/src/main/java/de/holisticon/util/tracee/contextlogger/json/generator/WatchdogCategoryCreator.java
+++ context-logger/src/main/java/de/holisticon/util/tracee/contextlogger/json/generator/WatchdogCategoryCreator.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import de.holisticon.util.tracee.contextlogger.json.beans.WatchdogCategory;
 import de.holisticon.util.tracee.contextlogger.json.generator.datawrapper.WatchdogDataWrapper;
+import de.holisticon.util.tracee.contextlogger.json.generator.util.RecursiveReflectionToStringStyle;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.aspectj.lang.reflect.MethodSignature;
 
@@ -26,7 +27,7 @@ public final class WatchdogCategoryCreator {
         // output parameters
         final List<String> parameters = new ArrayList<String>();
         for (final Object attr : watchdogDataWrapper.getProceedingJoinPoint().getArgs()) {
-            parameters.add(attr == null ? null : ReflectionToStringBuilder.reflectionToString(attr));
+            parameters.add(attr == null ? null : ReflectionToStringBuilder.reflectionToString(attr,new RecursiveReflectionToStringStyle()));
         }
 
         // output called instance
@@ -35,7 +36,7 @@ public final class WatchdogCategoryCreator {
         String deSerializedInstance;
         Object targetInstance = watchdogDataWrapper.getProceedingJoinPoint().getTarget();
         if (targetInstance != null) {
-            deSerializedInstance = ReflectionToStringBuilder.reflectionToString(targetInstance);
+            deSerializedInstance = ReflectionToStringBuilder.reflectionToString(targetInstance, new RecursiveReflectionToStringStyle());
         } else {
             deSerializedInstance = null;
         }
---
+++ context-logger/src/main/java/de/holisticon/util/tracee/contextlogger/json/generator/util/RecursiveReflectionToStringStyle.java
@@ -0,0 +1,51 @@
+package de.holisticon.util.tracee.contextlogger.json.generator.util;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.Collection;
+
+/**
+ * Created by Tobias Gindler, holisticon ag on 20.02.14.
+ */
+public class RecursiveReflectionToStringStyle extends ToStringStyle{
+
+        private static final int    MAX_DEPTH  = 8;
+        private static final int    DEFAULT_DEPTH = 5;
+
+        private int                 maxDepth;
+        private int                 depth;
+
+        public RecursiveReflectionToStringStyle() {
+            this(DEFAULT_DEPTH);
+        }
+
+        public RecursiveReflectionToStringStyle(int maxDepth) {
+            setUseShortClassName(true);
+            setUseIdentityHashCode(true);
+
+            this.maxDepth = maxDepth > MAX_DEPTH ? MAX_DEPTH : maxDepth;
+        }
+
+        @Override
+        protected void appendDetail(StringBuffer buffer, String fieldName, Object value) {
+            if (value.getClass().getName().startsWith("java.lang.")
+                    || (depth >= maxDepth)) {
+                buffer.append(value);
+            }
+            else {
+                depth++;
+                buffer.append(ReflectionToStringBuilder.toString(value, this, true, true));
+                depth--;
+            }
+        }
+
+        // another helpful method
+        @Override
+        protected void appendDetail(StringBuffer buffer, String fieldName, Collection<?> coll) {
+            depth++;
+            buffer.append(ReflectionToStringBuilder.toString(coll.toArray(), this, true, true));
+            depth--;
+        }
+
+}
