933e5ee0a748d2d411e363b0e005fb0c0257b44e
Daniel Wegener <daniel.wegener@holisticon.de>
2014-02-21 18:05:28 +0100

    Tests for Utilities

--- core/src/main/java/de/holisticon/util/tracee/Utilities.java
+++ core/src/main/java/de/holisticon/util/tracee/Utilities.java
@@ -1,9 +1,5 @@
 package de.holisticon.util.tracee;
 
-import java.nio.charset.Charset;
-import java.nio.charset.UnsupportedCharsetException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Random;
@@ -15,9 +11,10 @@ public final class Utilities {
 
 	private static final char[] ALPHANUMERICS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
 
-	private Utilities() {
-		// hide constructor
-	}
+    private Utilities() {
+        // hide constructor
+    }
+
 
 	public static boolean isNullOrEmptyString(String value) {
 		return value == null || value.trim().isEmpty();
@@ -54,31 +51,14 @@ public final class Utilities {
 		return new String(randomChars);
 	}
 
-	/**
-	 * Creates a alphanumeric projection with a given length of the given object using its {@link Object#hashCode()}
-	 */
-	public static String createAlphanumericHash(String str, int length) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			final byte[] digest = md.digest(str.getBytes(Charset.forName("UTF-8")));
-			// To human
-			StringBuilder sb = new StringBuilder();
-			for (byte b : digest) {
-				if (b < 16) sb.append("0");
-				sb.append(Integer.toHexString(b & 0xff));
-			}
-			// repeat if to small
-			while (sb.length() < length) {
-				sb.append(sb.toString());
-			}
-			// truncation and return
-			return sb.delete(length, sb.length()).toString();
-		} catch (NoSuchAlgorithmException e) {
-			// Preferred hash algorithm is not available. We generate random string.
-			return createRandomAlphanumeric(length);
-		} catch (UnsupportedCharsetException e) {
-			// We should handle such error like the NoSuchAlgorithmException
-			return createRandomAlphanumeric(length);
-		}
-	}
+    /**
+     * Creates a alphanumeric projection with a given length of the given object using its {@link Object#hashCode()}
+     */
+    public static String createAlphanumericHash(Object o, int length) {
+        final int hashCode = o.hashCode();
+
+        // TODO using seeds is not supported - a UnsupportedOperationException will be thrown
+        // ThreadLocalRandom.current().setSeed(hashCode);
+        return createRandomAlphanumeric(length);
+    }
 }
--- core/src/test/java/de/holisticon/util/tracee/UtilitiesTest.java
+++ core/src/test/java/de/holisticon/util/tracee/UtilitiesTest.java
@@ -2,50 +2,31 @@ package de.holisticon.util.tracee;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Daniel Wegener (Holisticon AG)
  */
 public class UtilitiesTest {
 
+    @Test
+    public void testCreateRandomAlphanumeric() {
+        final String randomAlphanumeric = Utilities.createRandomAlphanumeric(32);
+		assertTrue("32 alphanumeric character sequence",
+				Pattern.compile("[0-9a-zA-Z]{32}").matcher(randomAlphanumeric).matches());
+	}
+
 	@Test
-	public void returnTrueIfNull() {
+	public void testIsNullOrEmptyString() {
 		assertThat(Utilities.isNullOrEmptyString(null), is(true));
-	}
-	
-	@Test
-	public void returnTrueIfEmpty() {
 		assertThat(Utilities.isNullOrEmptyString(""), is(true));
-	}
-
-	@Test
-	public void returnTrueIfBlank() {
 		assertThat(Utilities.isNullOrEmptyString(" "), is(true));
-	}
-	
-	public void returnFalseIfSet() {
-		assertThat(Utilities.isNullOrEmptyString("A"), is(false));
-	}
-	
-	@Test
-	public void shouldTransformToEnumeration() {
-		final Enumeration<String> enuma = Utilities.toEnumeration(Arrays.asList("A", "B", "C"));
-		assertThat(enuma.hasMoreElements(), is(true));
-		assertThat(enuma.nextElement(), is(equalTo("A")));
-		assertThat(enuma.nextElement(), is(equalTo("B")));
-		assertThat(enuma.nextElement(), is(equalTo("C")));
-	}
-
-	@Test
-	public void shouldTransformEmptyList() {
-		final Enumeration<?> enuma = Utilities.toEnumeration(Collections.emptyList());
-		assertThat(enuma.hasMoreElements(), is(false));
+		assertThat(Utilities.isNullOrEmptyString(" \n\t"), is(true));
+		assertThat(Utilities.isNullOrEmptyString("a"), is(false));
 	}
 
 	@Test
