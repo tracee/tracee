407d9bf8b0f9a1f426182e3f5c90312ce0fedca6
Daniel Wegener <daniel.wegener@holisticon.de>
2014-02-24 22:48:41 +0100

    docker configuration for jbossas7

---
+++ examples/docker/README.md
@@ -0,0 +1,8 @@
+> This document contains documentation for TracEE examples. Click [here](/README.md) to get an overview that TracEE is about.
+
+# tracee-examples
+
+This folder contains docker descriptors for tracee-examples. It starts a preconfigured tomcat7 and jbossas7 and
+deploys tracee-examples-webapp and tracee-examples-ear into these containers.
+
+!![Gliffy](docker_gliffy.svg)
--- examples/docker/startEnvironment.sh
+++ examples/docker/startEnvironment.sh
@@ -4,5 +4,5 @@ docker run -d -p 27017:27017 -p 28017:28017 --name=tracee-example-graylog-mongod
 docker run -d -p 9200:9200 -p 9300:9300 --name=tracee-example-graylog-elasticsearch-1 tracee-example-graylog-elasticsearch
 docker run -d -p 12900:12900 -link tracee-example-graylog-mongodb-1:mongo -link tracee-example-graylog-elasticsearch-1:elasticsearch --name=tracee-example-graylog-server-1 tracee-example-graylog-server
 docker run -d -p 9000:9000 -link tracee-example-graylog-server-1:graylog --name=tracee-example-graylog-web-1 tracee-example-graylog-web
-docker run -d -p 8080:8080 -link tracee-example-graylog-server-1:graylog --name=tracee-example-tomcat7-1 tracee-example-tomcat7
-
+docker run -d -p 8081:8080 -link tracee-example-graylog-server-1:graylog --name=tracee-example-jbossas7-1 tracee-example-jbossas7
+docker run -d -p 8080:8080 -link tracee-example-graylog-server-1:graylog -link tracee-example-jbossas7-1:jbossas7 --name=tracee-example-tomcat7-1 tracee-example-tomcat7
