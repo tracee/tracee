ad0dbdd513bf781e54ca984f1b6827ddaf0a2347
Sven Bunge <sven.bunge@holisticon.de>
2014-03-10 09:50:37 +0100

    #5: Add additional tests

--- slf4j/src/main/java/de/holisticon/util/tracee/backend/slf4j/Slf4jTraceeLogger.java
+++ slf4j/src/main/java/de/holisticon/util/tracee/backend/slf4j/Slf4jTraceeLogger.java
@@ -15,38 +15,46 @@ public final class Slf4jTraceeLogger implements TraceeLogger {
     private final Logger logger;
 
     public Slf4jTraceeLogger(final Class<?> clazz) {
-        this.logger = LoggerFactory.getLogger(clazz);
+        this(LoggerFactory.getLogger(clazz));
     }
+	
+	Slf4jTraceeLogger(final Logger logger) {
+		this.logger = logger;
+	}
 
     public void debug(Object message) {
-        logger.debug(message != null ? message.toString() : "");
+        logger.debug(nullsafeString(message));
     }
 
     public void debug(Object message, Throwable t) {
-        logger.debug(message != null ? message.toString() : "", t);
+        logger.debug(nullsafeString(message), t);
     }
 
     public void error(Object message) {
-        logger.error(message != null ? message.toString() : "");
+        logger.error(nullsafeString(message));
     }
 
     public void error(Object message, Throwable t) {
-        logger.error(message != null ? message.toString() : "", t);
+        logger.error(nullsafeString(message), t);
     }
 
     public void info(Object message) {
-        logger.info(message != null ? message.toString() : "");
+        logger.info(nullsafeString(message));
     }
 
     public void info(Object message, Throwable t) {
-        logger.info(message != null ? message.toString() : "", t);
+        logger.info(nullsafeString(message), t);
     }
 
     public void warn(Object message) {
-        logger.warn(message != null ? message.toString() : "");
+        logger.warn(nullsafeString(message));
     }
 
-    public void warn(Object message, Throwable t) {
-        logger.warn(message != null ? message.toString() : "", t);
+	public void warn(Object message, Throwable t) {
+        logger.warn(nullsafeString(message), t);
     }
+
+	private String nullsafeString(Object message) {
+		return message != null ? message.toString() : "";
+	}
 }
