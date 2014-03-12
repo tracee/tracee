09cb1f3663392108d2ca112b8e38281abb8076b7
Daniel Wegener <daniel.wegener@holisticon.de>
2014-03-12 12:26:41 +0100

    fix #17 and add tests for tracee-log4j

--- log4j/src/main/java/de/holisticon/util/tracee/backend/log4j/Log4jTraceeBackendProvider.java
+++ log4j/src/main/java/de/holisticon/util/tracee/backend/log4j/Log4jTraceeBackendProvider.java
@@ -7,17 +7,17 @@ import de.holisticon.util.tracee.spi.TraceeBackendProvider;
 import java.util.Set;
 
 /**
- * @author Daniel
+ * @author Daniel Wegener (Holisticon AG)
  */
 public class Log4jTraceeBackendProvider implements TraceeBackendProvider {
 
-    private final Log4jMdcLikeAdapter log4jMdcLikeAdapter = new Log4jMdcLikeAdapter();
-    private final ThreadLocal<Set<String>> traceeKeyStore = new ThreadLocalHashSet<String>();
+	private final Log4jMdcLikeAdapter log4jMdcLikeAdapter = new Log4jMdcLikeAdapter();
+	private final ThreadLocal<Set<String>> traceeKeyStore = new ThreadLocalHashSet<String>();
 
-    private final Log4jTraceeBackend log4jTraceeBackend = new Log4jTraceeBackend(log4jMdcLikeAdapter, traceeKeyStore);
+	private final Log4jTraceeBackend log4jTraceeBackend = new Log4jTraceeBackend(log4jMdcLikeAdapter, traceeKeyStore);
 
-    @Override
-    public final TraceeBackend provideBackend() {
-        return log4jTraceeBackend;
-    }
+	@Override
+	public final TraceeBackend provideBackend() {
+		return log4jTraceeBackend;
+	}
 }
---
+++ log4j/src/test/java/de/holisticon/util/tracee/backend/log4j/Log4jTraceeLoggerTest.java
@@ -0,0 +1,70 @@
+package de.holisticon.util.tracee.backend.log4j;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Daniel Wegener (Holisticon AG)
+ */
+public class Log4jTraceeLoggerTest {
+
+	private static final String MESSAGE = "TEST";
+	private static final Exception EXCEPTION = new RuntimeException("My exception");
+
+	private Logger mockedLogger = Mockito.mock(Logger.class);
+
+	private Log4jTraceeLogger UNIT = new Log4jTraceeLogger(mockedLogger);
+
+
+	@Test
+	public void logDebugMessageWithLogger() {
+		UNIT.debug(MESSAGE);
+		verify(mockedLogger).debug(MESSAGE);
+	}
+
+	@Test
+	public void logDebugMessageAndExceptionWithLogger() {
+		UNIT.debug(MESSAGE, EXCEPTION);
+		verify(mockedLogger).debug(MESSAGE, EXCEPTION);
+	}
+
+	@Test
+	public void logInfoMessageWithLogger() {
+		UNIT.info(MESSAGE);
+		verify(mockedLogger).info(MESSAGE);
+	}
+
+	@Test
+	public void logInfoMessageAndExceptionWithLogger() {
+		UNIT.info(MESSAGE, EXCEPTION);
+		verify(mockedLogger).info(MESSAGE, EXCEPTION);
+	}
+
+	@Test
+	public void logWarnMessageWithLogger() {
+		UNIT.warn(MESSAGE);
+		verify(mockedLogger).warn(MESSAGE);
+	}
+
+	@Test
+	public void logWarnMessageAndExceptionWithLogger() {
+		UNIT.warn(MESSAGE, EXCEPTION);
+		verify(mockedLogger).warn(MESSAGE, EXCEPTION);
+	}
+
+	@Test
+	public void logErrorMessageWithLogger() {
+		UNIT.error(MESSAGE);
+		verify(mockedLogger).error(MESSAGE);
+	}
+
+	@Test
+	public void logErrorMessageAndExceptionWithLogger() {
+		UNIT.error(MESSAGE, EXCEPTION);
+		verify(mockedLogger).error(MESSAGE, EXCEPTION);
+	}
+
+}
