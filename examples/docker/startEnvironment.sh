c4d302c7dbec8149044dc134a020d00dd712b787
Daniel Wegener <daniel.wegener@holisticon.de>
2014-04-24 16:25:08 +0200

    #16 replaces -link with --link commands in docker start scripts [skip ci]

--- examples/docker/startEnvironment.sh
+++ examples/docker/startEnvironment.sh
@@ -9,15 +9,15 @@ while ! nc -vz localhost 27017; do sleep 1; done
 while ! nc -vz localhost 28017; do sleep 1; done
 sleep 5s
 # Start graylog2-server
-docker run -d -p 12900:12900 -p 12201:12201/udp -link tracee-example-graylog-mongodb-1:mongo -link tracee-example-graylog-elasticsearch-1:elasticsearch -h="tracee-example-graylog-server-1" --name="tracee-example-graylog-server-1" tracee-example-graylog-server
+docker run -d -p 12900:12900 -p 12201:12201/udp -link tracee-example-graylog-mongodb-1:mongo --link tracee-example-graylog-elasticsearch-1:elasticsearch -h="tracee-example-graylog-server-1" --name="tracee-example-graylog-server-1" tracee-example-graylog-server
 curl -XPOST http://admin:yummie@127.0.0.1:12900/system/inputs -d @graylog2-server-inputs.json -H "Content-Type:application/json"
 while [ $? -ne 0 ]; do
 	sleep 2
     curl -XPOST http://admin:yummie@127.0.0.1:12900/system/inputs -d @graylog2-server-inputs.json -H "Content-Type:application/json"
 done
 # Start graylog2-server-web
-docker run -d -p 9000:9000 -link tracee-example-graylog-server-1:graylog -h="tracee-example-graylog-web-1" --name="tracee-example-graylog-web-1" tracee-example-graylog-web
+docker run -d -p 9000:9000 --link tracee-example-graylog-server-1:graylog -h="tracee-example-graylog-web-1" --name="tracee-example-graylog-web-1" tracee-example-graylog-web
 # Start jbossas7
-docker run -d -p 8081:8080 -p 9990:9990 -link tracee-example-graylog-server-1:graylog -h="tracee-example-jbossas7-1" --name="tracee-example-jbossas7-1" tracee-example-jbossas7
+docker run -d -p 8081:8080 -p 9990:9990 --link tracee-example-graylog-server-1:graylog -h="tracee-example-jbossas7-1" --name="tracee-example-jbossas7-1" tracee-example-jbossas7
 # Start tomcat7
-docker run -d -p 8080:8080 -link tracee-example-graylog-server-1:graylog -link tracee-example-jbossas7-1:jbossas7 -h="tracee-example-tomcat7-1" --name="tracee-example-tomcat7-1" tracee-example-tomcat7
+docker run -d -p 8080:8080 --link tracee-example-graylog-server-1:graylog --link tracee-example-jbossas7-1:jbossas7 -h="tracee-example-tomcat7-1" --name="tracee-example-tomcat7-1" tracee-example-tomcat7
