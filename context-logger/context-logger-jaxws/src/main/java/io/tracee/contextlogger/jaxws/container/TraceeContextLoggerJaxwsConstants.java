1d578fa78e5e5320e4c1f465484e621dbe244128
Sven Bunge <sven.bunge@nnga.de>
2014-06-01 17:58:02 +0200

    Adjust path: If the file is assembled with the xml file into a jar file, the project structure is discarded.

--- context-logger/context-logger-jaxws/src/main/java/io/tracee/contextlogger/jaxws/container/TraceeContextLoggerJaxwsConstants.java
+++ context-logger/context-logger-jaxws/src/main/java/io/tracee/contextlogger/jaxws/container/TraceeContextLoggerJaxwsConstants.java
@@ -10,6 +10,6 @@ public final class TraceeContextLoggerJaxwsConstants {
 	private TraceeContextLoggerJaxwsConstants() { }
 
 	public static final String TRACEE_WITH_ERROR_LOGGING_HANDLER_CHAIN_URL =
-			"/src/main/resources/io/tracee/contextlogger/jaxws/TraceeWithErrorLoggingHandlerChain.xml";
+			"/io/tracee/contextlogger/jaxws/TraceeWithErrorLoggingHandlerChain.xml";
 
 }
