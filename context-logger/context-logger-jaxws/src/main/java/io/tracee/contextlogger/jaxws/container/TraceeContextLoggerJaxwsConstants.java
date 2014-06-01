f33fc6791d5b4c5bf097566e6c5015101d57ff85
Sven Bunge <sven.bunge@nnga.de>
2014-06-01 17:54:56 +0200

    Add comment and SuppressWarning annotation

--- context-logger/context-logger-jaxws/src/main/java/io/tracee/contextlogger/jaxws/container/TraceeContextLoggerJaxwsConstants.java
+++ context-logger/context-logger-jaxws/src/main/java/io/tracee/contextlogger/jaxws/container/TraceeContextLoggerJaxwsConstants.java
@@ -1,5 +1,10 @@
 package io.tracee.contextlogger.jaxws.container;
 
+/**
+ * This class is not directly used but exists for the users convenience
+ * (Like adding @HandlerChain(xmlFile=TraceeContextLoggerJaxwsConstants.TRACEE_WITH_ERROR_LOGGING_HANDLER_CHAIN_URL)
+ */
+@SuppressWarnings("UnusedDeclaration")
 public final class TraceeContextLoggerJaxwsConstants {
 
 	private TraceeContextLoggerJaxwsConstants() { }
