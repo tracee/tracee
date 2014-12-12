75ab2a6c7b6964632eae38be703f82c2c96d10ee
Daniel Wegener <daniel@wegener.me>
2014-12-12 21:50:56 +0100

    Implements a spring.web ClientHttpRequestInterceptor. Partially solves #43 (integration test ist still missing)

---
+++ binding/springhttpclient/src/main/java/io/tracee/springhttpclient/TraceeClientHttpRequestInterceptor.java
@@ -0,0 +1,70 @@
+package io.tracee.springhttpclient;
+
+import io.tracee.Tracee;
+import io.tracee.TraceeBackend;
+import io.tracee.TraceeConstants;
+import io.tracee.configuration.TraceeFilterConfiguration;
+import io.tracee.transport.HttpJsonHeaderTransport;
+import io.tracee.transport.TransportSerialization;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static io.tracee.configuration.TraceeFilterConfiguration.Channel.IncomingResponse;
+import static io.tracee.configuration.TraceeFilterConfiguration.Channel.OutgoingRequest;
+
+public final class TraceeClientHttpRequestInterceptor implements ClientHttpRequestInterceptor {
+
+	private final TraceeBackend backend;
+	private final TransportSerialization<String> transportSerialization;
+	private final String profile;
+
+	public TraceeClientHttpRequestInterceptor(String profile) {
+		this.backend = Tracee.getBackend();
+		this.transportSerialization = new HttpJsonHeaderTransport(backend.getLoggerFactory());
+		this.profile = profile;
+	}
+
+	protected TraceeClientHttpRequestInterceptor(TraceeBackend backend, TransportSerialization<String> transportSerialization, String profile) {
+		this.backend = backend;
+		this.transportSerialization = transportSerialization;
+		this.profile = profile;
+	}
+
+
+	@Override
+	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+		preRequest(request);
+		final ClientHttpResponse response = execution.execute(request, body);
+		postResponse(response);
+		return response;
+	}
+
+	private void preRequest(HttpRequest request) {
+		final TraceeFilterConfiguration filterConfiguration = backend.getConfiguration(profile);
+		if (!backend.isEmpty() && filterConfiguration.shouldProcessContext(OutgoingRequest)) {
+			final Map<String, String> filteredParams = filterConfiguration.filterDeniedParams(backend, OutgoingRequest);
+			final String contextAsHeader = transportSerialization.render(filteredParams);
+			request.getHeaders().add(TraceeConstants.HTTP_HEADER_NAME, contextAsHeader);
+		}
+	}
+
+	private void postResponse(ClientHttpResponse response) {
+		final List<String> headers = response.getHeaders().get(TraceeConstants.HTTP_HEADER_NAME);
+		if (headers != null) {
+			final TraceeFilterConfiguration filterConfiguration = backend.getConfiguration(profile);
+
+			if (filterConfiguration.shouldProcessContext(IncomingResponse)) {
+				for (String header : headers) {
+					final Map<String, String> parsedContext = transportSerialization.parse(header);
+					backend.putAll(filterConfiguration.filterDeniedParams(parsedContext, IncomingResponse));
+				}
+			}
+		}
+	}
+}
---
+++ binding/springhttpclient/src/test/java/io/tracee/springhttpclient/TraceeClientHttpRequestInterceptorTest.java
@@ -0,0 +1,80 @@
+package io.tracee.springhttpclient;
+
+import io.tracee.*;
+import io.tracee.transport.HttpJsonHeaderTransport;
+import io.tracee.transport.TransportSerialization;
+import org.hamcrest.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class TraceeClientHttpRequestInterceptorTest {
+
+	private static final String USED_PROFILE = "A_PROFILE";
+	private final TransportSerialization<String> transportSerializationMock = new HttpJsonHeaderTransport(NoopTraceeLoggerFactory.INSTANCE);
+	private final TraceeBackend backend = new SimpleTraceeBackend(new PermitAllTraceeFilterConfiguration(), NoopTraceeLoggerFactory.INSTANCE);
+	private final TraceeClientHttpRequestInterceptor unit =
+			new TraceeClientHttpRequestInterceptor(backend, transportSerializationMock, USED_PROFILE);
+	private final byte[] payload = new byte[] {};
+	private final ClientHttpRequestExecution clientHttpRequestExecutionMock = mock(ClientHttpRequestExecution.class);
+
+	@Before
+	public void setupMocks() throws IOException {
+		when(clientHttpRequestExecutionMock.execute(any(HttpRequest.class), any(byte[].class))).thenAnswer(new Answer<ClientHttpResponse>() {
+			@Override
+			public ClientHttpResponse answer(InvocationOnMock invocation) throws Throwable {
+				final HttpHeaders headers = new HttpHeaders();
+				headers.add(TraceeConstants.HTTP_HEADER_NAME,"{\"fromResponse\":\"true\"}");
+				return new SimpleClientHttpResponse(HttpStatus.NO_CONTENT, "yawn", headers);
+			}
+		});
+
+	}
+
+	@Test
+	public void testInterceptPropagatesContextToRequest() throws Exception {
+		final HttpRequest request = new SimpleClientHttpRequestFactory().createRequest(URI.create("http://foo.bar"), HttpMethod.GET);
+		backend.put("inClientBeforeRequest", "true");
+		unit.intercept(request, payload, clientHttpRequestExecutionMock);
+		verify(clientHttpRequestExecutionMock).execute(argThat(headers(hasEntry(TraceeConstants.HTTP_HEADER_NAME, "{\"inClientBeforeRequest\":\"true\"}"))), any(byte[].class));
+	}
+
+	@Test
+	public void testInterceptReadsContextFromClientResponse() throws Exception {
+		final HttpRequest request = new SimpleClientHttpRequestFactory().createRequest(URI.create("http://foo.bar"), HttpMethod.GET);
+		unit.intercept(request, payload, clientHttpRequestExecutionMock);
+		assertThat(backend, hasEntry("fromResponse", "true"));
+
+	}
+
+	final Matcher<HttpRequest> headers(Matcher<Map<? extends String, ? extends String>> submatcher) {
+		return new FeatureMatcher<HttpRequest, Map<? extends String,? extends String>>(submatcher,"header", "header map") {
+			@Override
+			protected Map<String, String> featureValueOf(HttpRequest httpRequest) {
+				return httpRequest.getHeaders().toSingleValueMap();
+			}
+		};
+	}
+
+
+
+}
