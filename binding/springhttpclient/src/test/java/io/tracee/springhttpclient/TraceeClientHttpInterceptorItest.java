b7b175df2bd7c613e44bd9f087b64b79beaa9002
Daniel Wegener <daniel@wegener.me>
2014-12-14 21:07:33 +0100

    #43 add jetty integration test

---
+++ binding/springhttpclient/src/test/java/io/tracee/springhttpclient/TraceeClientHttpInterceptorItest.java
@@ -0,0 +1,70 @@
+package io.tracee.springhttpclient;
+
+import io.tracee.Tracee;
+import io.tracee.TraceeConstants;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TraceeClientHttpInterceptorItest {
+
+	private Server server;
+	private String serverEndpoint;
+
+	private static final class EmptyEntity {
+
+	}
+
+	@Test
+	public void testWritesToServerAndParsesResponse() throws IOException {
+		final RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setInterceptors(Arrays.<ClientHttpRequestInterceptor>asList(new TraceeClientHttpRequestInterceptor("default")));
+		Tracee.getBackend().put("beforeRequest", "yip");
+		restTemplate.getForObject(serverEndpoint, EmptyEntity.class);
+		assertThat(Tracee.getBackend().get("responseFromServer"), equalTo("yesSir"));
+	}
+
+	@Before
+	public void startJetty() throws Exception {
+		server = new Server(new InetSocketAddress("127.0.0.1", 0));
+		server.setHandler(requestHandler);
+		server.start();
+		serverEndpoint = "http://"+server.getConnectors()[0].getName();
+	}
+
+	private final Handler requestHandler = new AbstractHandler() {
+		@Override
+		public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+			final String incomingTraceeHeader = request.getHeader(TraceeConstants.HTTP_HEADER_NAME);
+			assertThat(incomingTraceeHeader, equalTo("{\"beforeRequest\":\"yip\"}"));
+			httpServletResponse.setHeader(TraceeConstants.HTTP_HEADER_NAME, "{ \"responseFromServer\":\"yesSir\" }");
+			httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+			request.setHandled(true);
+		}
+	};
+
+	@After
+	public void stopJetty() throws Exception {
+		if (server != null) {
+			server.stop();
+			server.join();
+		}
+	}
+
+}
