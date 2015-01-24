5e15443d1d4928e6023060625f2a1e7f69e35912
Sven Bunge <sven.bunge@holisticon.de>
2015-01-24 16:13:09 +0100

    Refactoring of jaxws integrationtest. Remove EJB stack and use JaxWS-stack provided by the JDK. Use SimpleTraceeBackend instead of two Backends in different ThreadLocal-stores.

--- binding/jaxws/src/test/java/io/tracee/binding/jaxws/TraceeJaxWsEndpointImpl.java
+++ binding/jaxws/src/test/java/io/tracee/binding/jaxws/TraceeJaxWsEndpointImpl.java
@@ -1,32 +1,29 @@
 package io.tracee.binding.jaxws;
 
 import io.tracee.Tracee;
-import org.apache.log4j.Logger;
 
-
-import javax.ejb.Stateless;
 import javax.jws.HandlerChain;
 import javax.jws.WebService;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-@Stateless
 @WebService(serviceName = TraceeJaxWsEndpointImpl.Descriptor.SERVICE_NAME, portName = "TraceeJaxWsEndpointPort",
         targetNamespace = TraceeJaxWsEndpoint.Descriptor.TNS,
         endpointInterface = "io.tracee.binding.jaxws.TraceeJaxWsEndpoint")
 @HandlerChain(file = TraceeWsHandlerConstants.TRACEE_HANDLER_CHAIN_URL)
 public class TraceeJaxWsEndpointImpl implements TraceeJaxWsEndpoint {
 
-    private final Logger LOGGER = Logger.getLogger(TraceeJaxWsEndpointImpl.class);
-
     @Override
     public List<String> getCurrentTraceeContext() {
 
         final List<String> entries = new ArrayList<String>();
-        LOGGER.info("Hello from Endpoint");
         for (Map.Entry<String, String> entry : Tracee.getBackend().copyToMap().entrySet()) {
             entries.add(entry.getKey());
             entries.add(entry.getValue());
         }
+
+		Tracee.getBackend().put("called", "yes");
 
         return entries;
     }
