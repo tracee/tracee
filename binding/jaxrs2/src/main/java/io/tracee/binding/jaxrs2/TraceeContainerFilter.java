36ba0a44187992f0255030222db8cc0552514a32
Sven Bunge <sven.bunge@holisticon.de>
2015-03-12 14:03:16 +0100

    Handle request and response in one class: Only one filter is used for client / server-side.

--- binding/jaxrs2/src/main/java/io/tracee/binding/jaxrs2/TraceeContainerFilter.java
+++ binding/jaxrs2/src/main/java/io/tracee/binding/jaxrs2/TraceeContainerFilter.java
@@ -8,29 +8,35 @@ import io.tracee.transport.HttpHeaderTransport;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import static io.tracee.configuration.TraceeFilterConfiguration.Channel.IncomingRequest;
+import static io.tracee.configuration.TraceeFilterConfiguration.Channel.OutgoingResponse;
 
 @Provider
-public class TraceeContainerRequestFilter implements ContainerRequestFilter {
+public class TraceeContainerFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
 	private final TraceeBackend backend;
 	private final HttpHeaderTransport transportSerialization;
 
 	@SuppressWarnings("unused")
-	public TraceeContainerRequestFilter() {
+	public TraceeContainerFilter() {
 		this(Tracee.getBackend());
 	}
 
-	TraceeContainerRequestFilter(TraceeBackend backend) {
+	TraceeContainerFilter(TraceeBackend backend) {
 		this.backend = backend;
 		this.transportSerialization = new HttpHeaderTransport(backend.getLoggerFactory());
 	}
 
+	/**
+	 * This method handles the incoming request
+	 */
 	@Override
 	public final void filter(final ContainerRequestContext containerRequestContext) throws IOException {
 
@@ -43,5 +49,18 @@ public class TraceeContainerRequestFilter implements ContainerRequestFilter {
 		}
 
 		Utilities.generateInvocationIdIfNecessary(backend);
+	}
+
+	/**
+	 * This method handles the outgoing response
+	 */
+	@Override
+	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
+		if (backend.getConfiguration().shouldProcessContext(OutgoingResponse)) {
+			final Map<String, String> filtered = backend.getConfiguration().filterDeniedParams(backend.copyToMap(), OutgoingResponse);
+			responseContext.getHeaders().putSingle(TraceeConstants.TPIC_HEADER, transportSerialization.render(filtered));
+		}
+
+		backend.clear();
 	}
 }
